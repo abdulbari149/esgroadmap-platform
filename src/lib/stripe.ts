8682784f292d1dcb2041c650ff674bcd6ba447e7
abdulbari149 <abdulbari131103@gmail.com>
2024-07-18 17:06:51 +0500

    fix: issue

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -6,12 +6,16 @@ import { Stripe } from 'stripe';
 let stripe: Stripe;
 
 if (process.env.NODE_ENV === 'production') {
-  stripe = new Stripe(env.STRIPE_SECRET_KEY)
+  stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+    apiVersion: '2024-06-20'
+  })
 } else {
   // @ts-ignore
   if (!global.stripe) {
     // @ts-ignore
-    global.stripe = new Stripe(env.STRIPE_SECRET_KEY)
+    global.stripe = new Stripe(env.STRIPE_SECRET_KEY, {
+      apiVersion: '2024-06-20'
+    })
   }
   // @ts-ignore
   stripe = global.stripe;
