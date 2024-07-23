e325fe10e438f0e8791489632948d3ba3ddadbdd
abdulbari149 <abdulbari131103@gmail.com>
2024-07-23 14:12:08 +0500

    feat: adjusting padding on dashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 const Page = () => {
 	return (
 		<div>
-			<p className="text-center pt-[120px] text-[40px]">
+			<p className="text-center pt-[50px] text-[40px]">
 				Welcome to{" "}
 				<span className="text-[#219e98] font-semibold">
 					Esgroadmap User Portal
